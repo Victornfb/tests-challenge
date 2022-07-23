53a97ef781f811f4ab51107e589862dd2260e211
Victornfb <victornfb@outlook.com>
2022-07-23 14:14:53 -0300

    Test: Add CreateUserUseCase test

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,41 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserError } from "./CreateUserError";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let createUserRepositoryInMemory: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+
+describe('Create User Use Case', () => {
+
+  beforeEach(() => {
+    createUserRepositoryInMemory = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(createUserRepositoryInMemory);
+  });
+
+  it('should be able to create a new user', async () => {
+    const result = await createUserUseCase.execute({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: 'fake-password'
+    });
+
+    expect(result).toHaveProperty('id')
+  });
+
+  it('should not be able to create a user with an used email', async () => {
+    await expect(async () => {
+      await createUserUseCase.execute({
+        name: 'John Doe',
+        email: 'john@doe.com',
+        password: 'fake-password'
+      });
+
+      await createUserUseCase.execute({
+        name: 'John Doe 2',
+        email: 'john@doe.com',
+        password: 'fake-password-2'
+      });
+    }).rejects.toBeInstanceOf(CreateUserError);
+  });
+
+});
